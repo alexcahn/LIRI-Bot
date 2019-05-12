e0a162fe24f21d4b47f4c73e54e144fb20a60160
Alex Cahn <alexbcahn@gmail>
2019-05-12 08:46:36 -0500

    dotenv

--- liri.js
+++ liri.js
@@ -1,5 +1,5 @@
 // info for spotify
-// require("dotenv").config();
+require("dotenv").config();
 var Spotify = require("node-spotify-api");
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
