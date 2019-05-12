cd67ffa535c19db45447e47094ad64f39f072aa9
Alex Cahn <alexbcahn@gmail>
2019-05-12 09:27:27 -0500

    spotify search working

--- liri.js
+++ liri.js
@@ -7,12 +7,19 @@ var axios = require('axios');
 
 var command = process.argv[2];
 var input = process.argv[3];
- 
-spotify.search({ type: 'track', query: 'All the Small Things', limit: 3 }, function(err, data) {
+  
+spotify.search({ type: 'track', query: input, limit: 3 }, function(err, data) {
   if (err) {
     return console.log('Error occurred: ' + err);
-  }
+  }else if (command == "spotify-this-song"){
+    var spotifyData = data.tracks.items[0];
+    return console.log( " Artist: " + spotifyData.artists[0].name + "\n",
+    "Song Name: " + spotifyData.name + "\n",
+    "Preview Link: " + spotifyData.preview_url + "\n",
+    "Album Name: " + spotifyData.album.name + "\n")
+}
  
-console.log(data); 
+// console.log(data.tracks.items); 
 });
+
 
